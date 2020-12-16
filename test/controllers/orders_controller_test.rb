0e274ca9ee841a38dc5f0835947c0163b33bfc91
Tomás Coelho <tomascoelho6@gmail.com>
2020-12-16 14:22:43 -0300

    fix controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,8 +1,22 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup { go_shopping }
+
   test "should get create" do
-    get orders_create_url
-    assert_response :success
+    assert_difference('Order.count') do
+      post orders_url
+    end
+
+    assert_equal('Pedido criado com sucesso', flash[:notice])
+    assert_redirected_to cart_url
+  end
+
+  private
+
+  def go_shopping
+    get cart_url
+    @cart = Cart.find(session[:cart_id])
+    @cart.cart_products << cart_products
   end
 end
