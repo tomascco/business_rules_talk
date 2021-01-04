0f157bbf70b052be73d7dbd0de25e8eaeb52e65d
Tomás Coelho <tomascoelho6@gmail.com>
2021-01-04 09:29:23 -0300

    add cypress config files

---
+++ cypress/support/commands.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+  return cy.get(`[data-test=${selector}]`, ...args);
+});
+
