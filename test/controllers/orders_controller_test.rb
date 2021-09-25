d2f720ed78d0c548fac9cd2a6f210d8b0d615b15
Tomás Coêlho <tomascoelho6@gmail.com>
2021-09-25 12:33:25 -0300

    Simplify orders_controller test

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+  def setup_cart
+    get cart_url
+
+    Cart.find(session[:cart_id])
+  end
+
   test "create order" do
     # Arrange
     cart = setup_cart
@@ -8,61 +14,26 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     cart.cart_products << cart_products(:minecraft, :stardew_valley)
 
     # Act
-    assert_difference(-> { Order.count }) do
-      post orders_url
-    end
+    post orders_url
 
     # Assert
     assert_redirected_to(cart_url)
+
     assert_equal('Pedido criado com sucesso', flash[:notice])
-
-    order = Order.last
-
-    expected_total_price = calculate_total(cart.cart_products)
-    assert_equal(expected_total_price, order.total_price)
-
-    assert_equal(cart.cart_products.size, order.order_products.size)
+    assert_nil(session[:cart_id])
   end
 
-  test "create empty order" do
+  test "invalid order" do
     # Arrange
     cart = setup_cart
 
     # Act
-    assert_no_difference(-> { Order.count }) do
-      post orders_url
-    end
+    post orders_url
 
     # Assert
+    assert_redirected_to(cart_url)
+
     assert_equal('Erro ao criar o pedido', flash[:notice])
-  end
-
-  test "create invalid quantity order" do
-    # Arrange
-    cart = setup_cart
-
-    cart.cart_products << cart_products(:invalid)
-
-    # Act
-    assert_no_difference(-> { Order.count }) do
-      post orders_url
-    end
-
-    # Assert
-    assert_equal('Erro ao criar o pedido', flash[:notice])
-  end
-
-  private
-
-  def setup_cart
-    get cart_url
-
-    Cart.find(session[:cart_id])
-  end
-
-  def calculate_total(cart_products)
-    cart_products.reduce(0) do |sum, cart_product|
-      sum += cart_product.quantity * cart_product.product.price
-    end
+    refute_nil(session[:cart_id])
   end
 end
